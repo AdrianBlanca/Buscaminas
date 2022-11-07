6123eb6733b23b74c0a45ad970a6966a91ddd742
Adrian <adrianblancaenriquez26@gmail.com>
2022-11-08 00:26:05 +0100

    Terminado

--- app/src/main/java/com/example/buscaminas/Activities/JugarActivity.java
+++ app/src/main/java/com/example/buscaminas/Activities/JugarActivity.java
@@ -2,12 +2,15 @@ package com.example.buscaminas.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +22,11 @@ public class JugarActivity extends AppCompatActivity {
     GridView gridViewMapa;
     int[][] casillas;
     int[][] numeros;
+    boolean[] accedido;
     int nColumns;
     int nBombas;
     TextView textViewBombas;
+    boolean marcar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +35,10 @@ public class JugarActivity extends AppCompatActivity {
         nivel = 'E';
         nColumns = 8;
         nBombas = 15;
+        marcar = false;
         textViewBombas = (TextView) findViewById(R.id.textViewBombas);
         textViewBombas.setText(String.valueOf(nBombas));
+
         Bundle parametros = this.getIntent().getExtras();
 
         if(parametros != null) {
@@ -61,7 +68,8 @@ public class JugarActivity extends AppCompatActivity {
 
         casillas = new int[nColumns][nColumns];
         numeros = new int[nColumns][nColumns];
-        rellenarMapa(casillas, nColumns);
+        accedido = new boolean[nColumns * nColumns];
+        rellenarMapa(casillas, accedido, nColumns);
         colocarBombas(numeros, nColumns, nBombas);
         colocarNumeros(numeros, nColumns);
 
@@ -73,17 +81,179 @@ public class JugarActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 ImageView imageView = (ImageView) view;
-                imageView.setImageResource(numeros[i / nColumns][i % nColumns]);
+                if(marcar) {
+                    TextView textView = (TextView) findViewById(R.id.textViewBombas);
+
+                    if(casillas[i / 8][i % 8] == R.drawable.marked) {
+                        imageView.setImageResource(R.drawable.unopened);
+                        casillas[i / 8][i % 8] = R.drawable.unopened;
+                        textView.setText(String.valueOf(Integer.parseInt(String.valueOf(textView.getText())) + 1));
+
+
+                    }
+
+                    else if(casillas[i / 8][i % 8] == R.drawable.unopened){
+                        imageView.setImageResource(R.drawable.marked);
+                        casillas[i / 8][i % 8] = R.drawable.marked;
+                        textView.setText(String.valueOf(Integer.parseInt(String.valueOf(textView.getText())) - 1));
+
+                        if(Integer.parseInt(String.valueOf(textView.getText())) == 0) {
+                            if(comprobarVictoria(numeros, casillas)) {
+                                LinearLayout linearLayoutFinPartida = (LinearLayout) findViewById(R.id.linearLayoutFinPartida);
+                                linearLayoutFinPartida.setVisibility(View.VISIBLE);
+
+                                TextView textViewFinPartida = (TextView) findViewById(R.id.textViewFinPartida);
+                                textViewFinPartida.setText("Has marcado todas las bombas. Enhorabuena!");
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+
+                else {
+                    switch(numeros[i / 8][i % 8]) {
+                        case R.drawable.opened:
+                            revelar(i);
+                            break;
+
+                        case R.drawable.one:
+                            imageView.setImageResource(R.drawable.one);
+                            accedido[i] = true;
+                            break;
+
+                        case R.drawable.two:
+                            imageView.setImageResource(R.drawable.two);
+                            accedido[i] = true;
+                            break;
+
+                        case R.drawable.three:
+                            imageView.setImageResource(R.drawable.three);
+                            accedido[i] = true;
+                            break;
+
+                        case R.drawable.four:
+                            imageView.setImageResource(R.drawable.four);
+                            accedido[i] = true;
+                            break;
+
+                        case R.drawable.five:
+                            imageView.setImageResource(R.drawable.five);
+                            accedido[i] = true;
+                            break;
+
+                        case R.drawable.six:
+                            imageView.setImageResource(R.drawable.six);
+                            accedido[i] = true;
+                            break;
+
+                        case R.drawable.seven:
+                            imageView.setImageResource(R.drawable.seven);
+                            accedido[i] = true;
+                            break;
+
+                        case R.drawable.eight:
+                            imageView.setImageResource(R.drawable.eight);
+                            accedido[i] = true;
+                            break;
+
+                        case R.drawable.bomb:
+                            imageView.setImageResource(R.drawable.bomb);
+                            accedido[i] = true;
+                            gridViewMapa.setOnItemClickListener(null);
+
+                            LinearLayout linearLayoutFinPartida = (LinearLayout) findViewById(R.id.linearLayoutFinPartida);
+                            linearLayoutFinPartida.setVisibility(View.VISIBLE);
+
+                            TextView textViewFinPartida = (TextView) findViewById(R.id.textViewFinPartida);
+                            textViewFinPartida.setText("Has detonado una bomba. Fin de la partida");
+                            textViewFinPartida.setTextColor(getResources().getColor(R.color.red));
+
+                            break;
+
+                    }
+
+                }
 
             }
         });
 
     }
 
-    private void rellenarMapa(int[][] casillas, int nColumns) {
+    public void onClickNuevaPartida(View v) {
+        Intent intent = new Intent(JugarActivity.this, JugarActivity.class);
+        intent.putExtra("nivel", nivel);
+        startActivity(intent);
+
+    }
+
+    public void onClickMenu(View v) {
+        Intent intent = new Intent(JugarActivity.this, InicioActivity.class);
+        intent.putExtra("nivel", nivel);
+        startActivity(intent);
+
+    }
+
+    private void revelar(int position) {
+        if((position >= 0 && position < nColumns * nColumns) && !accedido[position]) {
+            ImageView im = (ImageView) gridViewMapa.getChildAt(position);
+
+            int x, y;
+
+            x = position / nColumns;
+            y = position % nColumns;
+
+            im.setImageResource(numeros[x][y]);
+            casillas[x][y] = numeros[x][y];
+            accedido[position] = true;
+
+            if(numeros[x][y] == R.drawable.opened) {
+                if(position % nColumns != 0) {
+                    revelar(position - nColumns - 1);
+                    revelar(position - 1);
+                    revelar(position + nColumns - 1);
+
+                }
+
+                revelar(position - nColumns);
+                revelar(position + nColumns);
+
+                if(position % nColumns != nColumns - 1) {
+                    revelar(position - nColumns + 1);
+                    revelar(position + 1);
+                    revelar(position + nColumns + 1);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public void onClickMarcar(View v) {
+        ImageView imageView = (ImageView) findViewById(R.id.botonMarcar);
+
+        if(marcar)
+            imageView.setImageResource(R.drawable.marked);
+
+        else
+            imageView.setImageResource(R.drawable.marked_clicked);
+
+        marcar = !marcar;
+
+    }
+
+    private void rellenarMapa(int[][] casillas, boolean[] accedido, int nColumns) {
         for(int i = 0; i < nColumns; i++) {
-            for(int j = 0; j < nColumns; j++)
+            for(int j = 0; j < nColumns; j++) {
                 casillas[i][j] = R.drawable.unopened;
+                accedido[i + j] = false;
+
+            }
 
         }
 
@@ -183,6 +353,21 @@ public class JugarActivity extends AppCompatActivity {
         }
 
         return nBombas;
+    }
+
+    private boolean comprobarVictoria(int[][] numeros, int[][] casillas) {
+        boolean victoria = true;
+
+        for(int i = 0; i < nColumns; i++) {
+            for(int j = 0; j < nColumns; j++) {
+                if(!(numeros[i][j] == R.drawable.bomb && casillas[i][j] == R.drawable.marked))
+                    victoria = false;
+
+            }
+
+        }
+
+        return victoria;
     }
 
 }
